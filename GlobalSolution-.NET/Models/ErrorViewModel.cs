2ebff5a1a04a3c35cc7297017c02612df29fd7c0
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-31 15:21:40 -0300

    Add project files.

---
+++ GlobalSolution-.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GlobalSolution_.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
