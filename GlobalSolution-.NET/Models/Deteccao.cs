ee748f5a7dd837916aee7ba56135a14b658d56c8
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-31 18:45:59 -0300

    Criado model da tabela Deteccao

---
+++ GlobalSolution-.NET/Models/Deteccao.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GlobalSolution_.NET.Models
+{
+    [Table("Deteccao")]
+    public class Deteccao
+    {
+        [Key]
+        [Column("id_deteccao")]
+        public int id_deteccao { get; set; }
+
+        [Required(ErrorMessage = "Data é obrigatória!")]
+        [Column("data")]
+        [DataType(DataType.DateTime, ErrorMessage = "Data inválida.")]
+        [Display(Name = "Data de Detecção")]
+        public DateTime data {  get; set; }
+    }
+}
