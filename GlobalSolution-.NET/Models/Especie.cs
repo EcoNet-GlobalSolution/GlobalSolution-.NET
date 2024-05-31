467c3c432b27e230a03931547455a760abc4808a
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-31 18:46:07 -0300

    Criado model da tabela Especie

---
+++ GlobalSolution-.NET/Models/Especie.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GlobalSolution_.NET.Models
+{
+    [Table("Especie")]
+    public class Especie
+    {
+        [Key]
+        [Column("id_especie")]
+        public int id_especie {  get; set; }
+
+        [Required(ErrorMessage = "Nome comum da espécie é necessário.")]
+        [StringLength(100, ErrorMessage = "O nome comum não pode exceder 100 caracteres.")]
+        [Display(Name = "Nome comum")]
+        [Column("nome_comum")]
+        public string? nome_comum { get; set; }
+
+        [Required(ErrorMessage = "Espécie é necessário.")]
+        [StringLength(100, ErrorMessage = "A espécie não pode exceder 100 caracteres.")]
+        [Display(Name = "Espécie")]
+        [Column("especie")]
+        public string? especie { get; set; }
+    }
+}
