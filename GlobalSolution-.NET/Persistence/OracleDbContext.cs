689195147273be7d5b31cbf39662899fe3ab604d
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-31 22:48:51 -0300

    Criado OracleDbContext

---
+++ GlobalSolution-.NET/Persistence/OracleDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GlobalSolution_.NET.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GlobalSolution_.NET.Persistence
+{
+    public class OracleDbContext : DbContext
+    {
+        public OracleDbContext(DbContextOptions<OracleDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<CoordenadasModel> Coordenadas { get; set; }
+
+        public DbSet<DeteccaoModel> Deteccao { get; set; }
+
+        public DbSet<EspecieModel> Especie { get; set; }
+
+        public DbSet<TiporiscoModel> Tiporisco { get; set; }
+    }
+}
