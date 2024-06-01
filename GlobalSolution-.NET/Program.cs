41a2103615ce271c98f039b3e9d6fa6dcaff9eb2
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-05-31 22:49:39 -0300

    Configurado conexão com banco

--- GlobalSolution-.NET/Program.cs
+++ GlobalSolution-.NET/Program.cs
@@ -1,7 +1,11 @@
+using GlobalSolution_.NET.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<OracleDbContext>(o => o.UseOracle(builder.Configuration.GetConnectionString("OracleConnection")));
 
 var app = builder.Build();
 
